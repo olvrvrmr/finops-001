dc1d08208ec0cc5725e33281503ce662e3db64f9
Olivier Vermeir <olivier.vermeir@ignyte.be>
2024-12-03 13:17:47 +0100

    Refresh

--- src/lib/rate-limit.ts
+++
@@ -1,8 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit"
-import { Redis } from "@upstash/redis"
-
-export const rateLimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-})
-
